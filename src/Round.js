02f4083a2d23bbe5d16ba88ee635e3faca3e4a60
Lourdes Benites <lourdesben95@gmail.com>
2022-05-12 15:27:05 -0600

    Add class for round

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,27 @@
+const Turn = require('./Turn');
+
 class Round {
-    constructor() {
-        
+    constructor(deck) {
+        this.deck = deck;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+        this.currentTurn = '';
+        this.currentCard = deck.cards[0];
+    }
+
+    returnCurrentCard() {
+        return this.deck.cards[this.turns];
+    }
+
+    takeTurn(guess) {
+        const currentCard = this.returnCurrentCard();
+        const turn = new Turn(guess, currentCard);
+        this.turns ++;
+        turn.evaluateGuess();
+        if (turn.isCorrect === false) {
+          this.incorrectGuesses.push(currentCard.id);
+        }
+        return turn.giveFeedback();
     }
 }
 
